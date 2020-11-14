e9978b3a2812723ca9f3a056ddbe5375f2fdbbbe
VeroFlores <veritocalla@gmail.com>
2020-11-13 23:14:58 -0500

    Se instala chalk

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // module.exports = () => {
 //   // ...
-// 
+//
 function sum(a, b) {
   return a + b;
 }
 module.exports = sum;
 
 const chalk = require('chalk');
- 
+
 console.log(chalk.green('Hello world!'));
-
-
