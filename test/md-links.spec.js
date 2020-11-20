b3dadbec7aaf57b3772b3e281dafbe73c83da41b
VeroFlores <veritocalla@gmail.com>
2020-11-20 00:41:14 -0500

    Test para paths

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,8 +1,28 @@
-const sum = require('../src/index.js');
+const {
+  pathExist,
+  absolutePath,
+  convertToAbsolutePath,
+  isFile,
+  readFile,
+  readDirectory,
+  extMd,
+  getAllFiles,
+} = require('../src/md-links');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('Read the path', () => {
+  it('Should return a boolean', () => {
+    expect(pathExist('README.md')).toEqual('true');
+  });
+  it('Should return  a boolean', () => {
+    expect(absolutePath('README.md')).toEqual('true');
+  });
+  it('Should return an absolute path', () => {
+    expect(convertToAbsolutePath('prueba')).toBe(
+      'C:/Users/PC USER/Desktop/LABORATORIA/MD-LINKS/LIM013-fe-md-links/prueba',
+    );
+  });
 });
+
 // describe('mdLinks', () => {
 
 //   it('should...', () => {
