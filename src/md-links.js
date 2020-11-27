393054ecb4b011c7c2cf5209d942951709111c9f
VeroFlores <veritocalla@gmail.com>
2020-11-26 19:06:37 -0500

    modify export way

--- src/md-links.js
+++ src/md-links.js
@@ -2,18 +2,18 @@ const {
   pathExist,
   getLinksInFiles,
 } = require('./util');
-const checkLinks = require('./validate');
+const validate = require('./validate');
 
 // const note = 'C:/Users/PC USER/Desktop/LABORATORIA/MD-LINKS/LIM013-fe-md-links/prueb';
 
 module.exports = (path, options) => {
   const promise = new Promise((resolve, reject) => {
     if (pathExist(path) === false) {
-      reject(new Error('Enter new path'));
+      resolve('Enter new path');
     }
 
     if (pathExist(path) === true && options) {
-      resolve(checkLinks(path)).then((arr) => {
+      resolve(validate.checkLinks(path)).then((arr) => {
         resolve(arr);
       });
     } else {
