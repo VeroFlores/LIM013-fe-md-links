0790e1903dff7ce198d793648d37c623f3968c57
VeroFlores <veritocalla@gmail.com>
2020-11-30 15:31:58 -0500

    se modifica parametro reject de la promesa

--- src/md-links.js
+++ src/md-links.js
@@ -7,7 +7,7 @@ const validate = require('./options');
 // const note = 'C:/Users/PC USER/Desktop/LABORATORIA/MD-LINKS/LIM013-fe-md-links/prueb';
 
 module.exports = (path, options) => {
-  const promise = new Promise((resolve, reject) => {
+  const promise = new Promise((resolve) => {
     if (pathExist(path) === false) {
       resolve('Enter new path');
     }
