d79c4abf7fd3a12a4b33dfa4eeae5f9ee4b59db7
VeroFlores <veritocalla@gmail.com>
2020-12-01 19:34:13 -0500

    se modifica las condicionales if

--- src/cli.js
+++ src/cli.js
@@ -17,36 +17,54 @@ program
   .option('-s, --stats')
   .parse(process.argv);
 const argv = process.argv.slice(2);
-  if (argv.length === 1) {
-    mdLinks(argv[0], { validate: false }).then((res) => {
-      console.log(res);
-    }).catch((res) => {
-      console.log(res);
-    });
-  }
-  if (argv[1] === '--validate' && argv[2] === '--stats') {
-    mdLinks(argv[0], { validate: true }).then((res) => {
-      console.log(statsLinks(res));
-      console.log(statsValidate(res));
-    }).catch(() => {
-    });
-  }
-  if (argv[1] === '--stats' && argv[2] === '--validate') {
-    mdLinks(argv[0], { validate: true }).then((res) => {
-      console.log(statsValidate(res));
-    }).catch(() => {
-    });
-  }
-  if (argv[1] === '--validate') {
-    mdLinks(argv[0], { validate: true }).then((res) => {
-      console.log(res);
-    }).catch((res) => {
-      console.log(res);
-    });
-  }
-  if (argv[1] === '--stats') {
-    mdLinks(argv[0], { validate: false }).then((res) => {
-      console.log(statsLinks(res));
-    }).catch(() => {
-    });
-  }
+// switch (argv) {
+//   case argv[0]:
+//     mdLinks(argv[0], { validate: false }).then((res) => {
+//       console.log(res);
+//     }).catch((res) => {
+//       console.log(res);
+//     });
+//     break;
+//   case argv[1] && argv[2]:
+//     mdLinks(argv[0], { validate: true }).then((res) => {
+//       console.log(statsLinks(res));
+//       console.log(statsValidate(res));
+//     }).catch(() => {
+//     });
+//     break;
+//   default:
+//     console.log(`Lo lamentamos, por el momento no disponemos de ${expr}.`);
+// }
+if (argv.length === 1) {
+  mdLinks(argv[0], { validate: false }).then((res) => {
+    console.log(res);
+  }).catch((res) => {
+    console.log(res);
+  });
+} else if (argv[1] === '--validate' && argv[2] === '--stats') {
+  mdLinks(argv[0], { validate: true }).then((res) => {
+    console.log(statsLinks(res));
+    console.log(statsValidate(res));
+  }).catch(() => {
+  });
+} else
+if (argv[1] === '--stats' && argv[2] === '--validate') {
+  mdLinks(argv[0], { validate: true }).then((res) => {
+    console.log(statsLinks(res));
+    console.log(statsValidate(res));
+  }).catch(() => {
+  });
+} else
+if (argv[1] === '--validate') {
+  mdLinks(argv[0], { validate: true }).then((res) => {
+    console.log(res);
+  }).catch((res) => {
+    console.log(res);
+  });
+} else
+if (argv[1] === '--stats') {
+  mdLinks(argv[0], { validate: false }).then((res) => {
+    console.log(statsLinks(res));
+  }).catch(() => {
+  });
+}
