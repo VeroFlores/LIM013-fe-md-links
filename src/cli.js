39dad67696fb1fff5007ee9d74ef82fdf3fe0740
VeroFlores <veritocalla@gmail.com>
2020-12-02 21:03:28 -0500

    se anade estilos al cli

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const { yellow } = require('chalk');
 const chalk = require('chalk');
 const commander = require('commander'); // (normal include)
 const process = require('process');
@@ -16,16 +17,39 @@ program
   .option('-v, --validate')
   .option('-s, --stats')
   .parse(process.argv);
+
+const warningColor = chalk.keyword('orange');
+const blue = chalk.hex('#429CA8');
+const pink = chalk.hex('#F590C4');
+const lightBlue = chalk.hex('#78E6F5');
+const lightYellow = chalk.hex('#F5E35F');
+const darkYellow=chalk.hex('#A89F41');
+
 const argv = process.argv.slice(2);
-// switch (argv) {
-//   case argv[0]:
+// const options = argv[1];
+// switch (options) {
+//   case '':
 //     mdLinks(argv[0], { validate: false }).then((res) => {
 //       console.log(res);
 //     }).catch((res) => {
 //       console.log(res);
 //     });
 //     break;
-//   case argv[1] && argv[2]:
+//   case '--validate':
+//     mdLinks(argv[0], { validate: true }).then((res) => {
+//       console.log(res);
+//     }).catch((res) => {
+//       console.log(res);
+//     });
+//     break;
+//   case '--stats':
+//     mdLinks(argv[0], { validate: false }).then((res) => {
+//       console.log(statsLinks(res));
+//     }).catch(() => {
+//     });
+//     break;
+//   case '--stats --validate':
+//   case '--validate --stats':
 //     mdLinks(argv[0], { validate: true }).then((res) => {
 //       console.log(statsLinks(res));
 //       console.log(statsValidate(res));
@@ -33,31 +57,35 @@ const argv = process.argv.slice(2);
 //     });
 //     break;
 //   default:
-//     console.log(`Lo lamentamos, por el momento no disponemos de ${expr}.`);
+//     console.log('Ingresa lo siguiente');
 // }
 if (argv.length === 1) {
   mdLinks(argv[0], { validate: false }).then((res) => {
-    console.log(res);
-  }).catch((res) => {
-    console.log(res);
-  });
+    res.forEach((obj) => {
+      const printLinks = `${blue(argv[0])} ${pink(obj.href)} ${lightBlue(obj.text)}`;
+      console.log(printLinks);
+    });
+  }).catch((rejected) => console.log(warningColor(rejected)));
 } else if (argv[1] === '--validate' && argv[2] === '--stats') {
   mdLinks(argv[0], { validate: true }).then((res) => {
-    console.log(statsLinks(res));
-    console.log(statsValidate(res));
+    console.log(lightBlue(statsLinks(res)));
+    console.log(yellow(statsValidate(res)));
   }).catch(() => {
   });
 } else
 if (argv[1] === '--stats' && argv[2] === '--validate') {
   mdLinks(argv[0], { validate: true }).then((res) => {
-    console.log(statsLinks(res));
-    console.log(statsValidate(res));
+    console.log(lightBlue(statsLinks(res)));
+    console.log(yellow(statsValidate(res)));
   }).catch(() => {
   });
 } else
 if (argv[1] === '--validate') {
   mdLinks(argv[0], { validate: true }).then((res) => {
-    console.log(res);
+    res.forEach((obj) => {
+      const printValidate = `${blue(argv[0])} ${pink(obj.href)} ${darkYellow(obj.statusText)} ${lightYellow(obj.status)} ${lightBlue(obj.text)}`;
+      console.log(printValidate);
+    });
   }).catch((res) => {
     console.log(res);
   });
