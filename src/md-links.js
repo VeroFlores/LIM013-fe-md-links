1c6ed7a4f98f6946cc69ed54a67ee4cd0feb2555
VeroFlores <veritocalla@gmail.com>
2020-12-02 21:05:10 -0500

    se añade parametro reject a funcion md-links

--- src/md-links.js
+++ src/md-links.js
@@ -7,9 +7,9 @@ const { checkLinks } = require('./options');
 // const note = 'C:/Users/PC USER/Desktop/LABORATORIA/MD-LINKS/LIM013-fe-md-links/prueb';
 
 module.exports = (path, options) => {
-  const promise = new Promise((resolve) => {
+  const promise = new Promise((resolve, reject) => {
     if (pathExist(path) === false) {
-      resolve('Enter new path');
+      reject(new Error('Enter new path'));
     }
     if (pathExist(path) === true && options.validate === true) {
       resolve(checkLinks(getLinksInFiles(path)));
