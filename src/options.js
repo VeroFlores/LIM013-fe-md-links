2573484a2eaa2de72cca17d55c7ba4fa55ba0753
VeroFlores <veritocalla@gmail.com>
2020-12-07 17:27:42 -0500

    se cambia a map en vez de forEach para recorrer array

--- src/options.js
+++ src/options.js
@@ -1,21 +1,17 @@
 const axios = require('axios');
 
 const checkLinks = (arrLink) => {
-  const arrValidateLinks = [];
-  arrLink.forEach((element) => {
-    arrValidateLinks.push(axios.get(element.href).then((response) => ({
-      ...element,
-      status: response.status,
-      statusText: response.statusText,
-    }))
-      .catch(() => ({ ...element, status: 404, statusText: 'FAIL' })));
-  });
+  const arrValidateLinks = arrLink.map((element) => (axios.get(element.href).then((response) => ({
+    ...element,
+    status: response.status,
+    statusText: response.statusText,
+  }))
+    .catch(() => ({ ...element, status: 404, statusText: 'FAIL' }))));
   return Promise.all(arrValidateLinks);
 };
 
 const statsLinks = (arr) => {
-  const links = [];
-  arr.map((arrObj) => links.push(arrObj.href));
+  const links = arr.map((arrObj) => arrObj.href);
   const total = links.length;
   const unique = [...new Set(links)].length;
   return `✔ Total : ${total}\n✔ Unique : ${unique}`;
