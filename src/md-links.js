adbe8247578d88ca39e5c4bec8d96d8372733a3c
VeroFlores <veritocalla@gmail.com>
2020-12-09 21:34:31 -0500

    se añade condiciones a funcion mdLinks

--- src/md-links.js
+++ src/md-links.js
@@ -1,21 +1,36 @@
 const {
   pathExist,
-  getLinksInFiles,
+  convertToAbsolutePath,
+  getLinks,
   getAllFiles,
 } = require('./util');
 const { checkLinks } = require('./options');
 
-// const note = 'C:/Users/PC USER/Desktop/LABORATORIA/MD-LINKS/LIM013-fe-md-links/prueb';
-
 module.exports = (path, options) => {
   const promise = new Promise((resolve, reject) => {
-    if (pathExist(path) === false) {
-      reject(new Error(`${path} doesn't exist\nEnter a new path`));
-    } else if (pathExist(path) === true && options.validate === true) {
-      resolve(checkLinks(getLinksInFiles(path)));
+    const absPath = convertToAbsolutePath(path);
+    if (pathExist(absPath) === true) {
+      if (getAllFiles(absPath).length === 0) {
+        reject(new Error(`${path} doesn't have md. files`));
+      }
+      if (getLinks(absPath).length === 0) {
+        reject(new Error(`${path} doesn't have links files`));
+      }
+      if (options.validate === true) {
+        resolve(checkLinks(getLinks(absPath)));
+      } else {
+        resolve(getLinks(path));
+      }
     } else {
-      resolve(getLinksInFiles(path));
+      reject(new Error(`${path} doesn't exist\nEnter a new path`));
     }
+    // if (pathExist(path) === false) {
+    //   reject(new Error(`${path} doesn't exist\nEnter a new path`));
+    // } else if (pathExist(path) === true && options.validate === true) {
+    //   resolve(checkLinks(getLinksInFiles(getAllFiles(path))));
+    // } else {
+    //   resolve(getLinksInFiles(getAllFiles(path)));
+    // }
   });
   return promise;
 };
